66236afbfcc2021404dfc447b93755ee87a9da2f
lillianvmcbride <76796155+lillianvmcbride@users.noreply.github.com>
2021-01-26 11:33:27 -0600

    initial event handlers

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 'use strict';
 console.log('Ready to Race!');
+
+function handleUserName(event) {
+  event.preventDefault();
+  var userNameInput = document.getElementById('username');
+  var userName = useNameInput.value;
+}
+
+function handleInputWords(event) {
+  event.preventDefault();
+  var wordInput = document.getElementById('wordInput');
+  var userNameInput = wordInput.value;
+}
+
+var usersName = document.getElementById('inputUserName');
+usersName = document.getElementById('submit', handleInputWords);
+
+var userInput = document.getElementById('inputWords');
+userInput.addEventListener('submit', handleUserName);
