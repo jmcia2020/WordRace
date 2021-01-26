e3e774ac448caa8386f4364bee1200a42a9b70fe
Collin Zeets <ckzeets@gmail.com>
2021-01-26 11:31:16 -0600

    added logic for word validation

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
+/* global words */
 "use strict";
 console.log("Ready to Race!");
+let userInput = "";
+let score = 0;
+console.log(words);
+function wordsValidate() {
+  if (words.indexOf(userInput.toLowerCase()) > -1) {
+    score + 100;
+    return true;
+  } else {
+    return false;
+  }
+}
