177e8fc48fb3e037c5b386e7614c30013f3d8058
lillianvmcbride <76796155+lillianvmcbride@users.noreply.github.com>
2021-01-26 12:28:59 -0600

    newUser

--- js/app.js
+++ js/app.js
@@ -13,24 +13,11 @@ function wordsValidate() {
   }
 }
 
-//button
-// Get the input field
-var input = document.getElementById("myInput");
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function (event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("myBtn").click();
-  }
-});
-
 function handleUserName(event) {
   event.preventDefault();
   var userNameInput = document.getElementById("username");
-  var userName = useNameInput.value;
+  var userName = userNameInput.value;
+  loadUser(userName);
 }
 
 function handleInputWords(event) {
@@ -58,10 +45,7 @@ User.prototype.saveToLocalStorage = function () {
   localStorage.setItem(this.name, JSON.stringify(this.scores));
 };
 
-/*
-use this function to load the user data for the leaderboards and for the game
   function loadUser(name) {
     var scores = JSON.parse(localStorage.getItem(name.toUpperCase())) || [];
     user = new User(name,scores);
   }
-*/
