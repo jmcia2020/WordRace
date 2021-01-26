005ef9276d669c2f0fa6d0153b8825f74e78266d
Vavyo <vavyo2121@gmail.com>
2021-01-26 10:40:00 -0600

    User constructor and saving to local storage

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
-'use strict';
-console.log('Ready to Race!');
+"use strict";
+console.log("Ready to Race!");
+
+var User = function (name, scores) {
+  this.name = name;
+  this.scores = scores;
+};
+
+User.prototype.addScore = function (score) {
+  this.scores.push(score);
+};
+
+User.prototype.saveToLocalStorage = function () {
+  localStorage.setItem(this.name, JSON.stringify(this.scores));
+};
+
+/*
+use this function to load the user data for the leaderboards and for the game
+  function loadUser(name) {
+    var scores = JSON.parse(localStorage.getItem(name.toUpperCase())) || [];
+    user = new User(name,scores);
+  }
+*/
