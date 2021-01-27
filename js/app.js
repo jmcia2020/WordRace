d0657e99f5dc06929fddf2010eae2c83164724fd
jmcia2020 <jeanmmcmahon@gmail.com>
2021-01-27 17:51:46 -0600

    added stopwatch and lillys changes

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@
 "use strict";
 console.log("Ready to Race!");
 var userInput = '';
-var currentPlayer;
+var currentPlayer = 0;
 var users = [];
+var displayWords =[];
 
+// This function validates the words against the words.js file
 function wordsValidate() {
   if (words.indexOf(userInput.toLowerCase()) > -1) {
     users[currentPlayer].scoring();
@@ -15,34 +17,43 @@ function wordsValidate() {
   }
 }
 
+// This function is an event handler that takes in the userName and changes it to UpperCase
 function handleUserName(event) {
   event.preventDefault();
+  var form = document.getElementById('inputUserName'); 
   var userNameInput = document.getElementById('username');
   var userName = userNameInput.value;
+  console.log(userName)
   userName = userName.toUpperCase();
   users.push(loadUser(userName));
+  form.textContent = "welcome" + userName;
 }
 
+// This function is an event handler that changes the wordInput to lowerCase for validation
 function handleInputWords(event) {
   event.preventDefault();
+  var form2 = document.getElementById('inputWords');
   var wordInput = document.getElementById('wordInput');
   userInput = wordInput.value;
   userInput = userInput.toLowerCase();
+  console.log(userInput);
   wordsValidate();
+  form2.reset();
   //updateScore();
 }
 
 var usersName = document.getElementById("inputUserName");
-usersName = document.getElementById("submit", handleInputWords);
+usersName = document.getElementById("submit", handleUserName);
 
 var userInput = document.getElementById("inputWords");
-userInput.addEventListener("submit", handleUserName);
+userInput.addEventListener("submit", handleInputWords);
 
 var User = function (name, scores) {
   this.name = name;
   this.scores = scores;
   this.currentScore;
 };
+
 
 User.prototype.scoring = function () {
   this.currentScore += 50;
